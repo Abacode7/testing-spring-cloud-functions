36a572231ec357c15c6d8609919a5b0db9f7d16c
Abraham Olamipo <aolamipo@teamapt.com>
2022-11-16 14:33:13 +0100

    Testing functional interfaces

--- src/main/java/com/pluralsight/webfunction/WebFunctionApplication.java
+++ src/main/java/com/pluralsight/webfunction/WebFunctionApplication.java
@@ -2,11 +2,16 @@ package com.pluralsight.webfunction;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class WebFunctionApplication {
@@ -28,6 +33,35 @@ public class WebFunctionApplication {
 			.findAny()
 			.orElse(null);
 			
+		};
+	}
+
+	@Bean
+	public Consumer<TollRecord> processTollRecord(){
+		return value -> {
+			System.out.println("received toll record with license plate number - " + value.getLicensePlate());
+		};
+	}
+
+	@Bean
+	public Function<TollRecord, Mono<Void>> processTollRecordReactive(){
+		return value -> {
+			System.out.println("received toll record with license plate number - " + value.getLicensePlate());
+			return Mono.empty();
+		};
+	}
+
+	@Bean
+	public Consumer<Flux<TollRecord>> processListOfTollRecordsReactive(){
+		return value -> {
+			value.subscribe(tollRecord -> System.out.println("received toll record with license - " + tollRecord.getLicensePlate()));
+		};
+	}
+
+	@Bean
+	public Supplier<Flux<TollStation>> getTollStations(){
+		return () -> {
+			return Flux.fromIterable(tollStations);
 		};
 	}
 
